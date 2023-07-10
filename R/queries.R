bbbf57ac44c925023817c3c3bd9f245dca63ad88
Matthew Harris <matthew.harris@unsw.edu.au>
2023-07-10 14:24:08 +1000

    add query functions

--- R/queries.R
+++ R/queries.R
@@ -1,5 +1,94 @@
 # Functions related to generating, reading and writing queries
 
+#' Move a query file.
+#'
+#' @description A wrapper for file.copy.
+#'
+#' @param from_file Full file path to the file to be moved.
+#' @param to_dir Full path to the desired destination directory.
+#' @param delete_original TRUE/FALSE to remove the original file.
+#'
+#' @noRd
+#'
+move_query <- function(from_file,
+                       to_dir,
+                       delete_original = TRUE){
+  # Test for presence of slash at end of directory
+  if(substr(to_dir, nchar(to_dir)-1+1, nchar(to_dir)) != "/"){
+    to_dir <- paste0(to_dir,"/")
+  }
+  # Trim the filename
+  file_short <- trim_path(filename = from_file)
+  # Add it to the to_dir
+  to_file = paste0(to_dir,file_short)
+  # Copy!
+  file.copy(from = from_file,
+            to = to_file)
+  # Delete, if asked.
+  if(isTRUE(delete_original)){
+    file.remove(from_file)
+  }
+}
+
+#' Convert a parsed query into a request for ecmwfr.
+#'
+#' @description Takes an output from parse_met_query and convert it into the format used by ecmwfr to fetch ERA-5 data.
+#'
+#' @param query an output from parse_met_query.
+#'
+#' @noRd
+#'
+query_to_CDSrequest <- function(query){
+  query_request <- list(
+    "dataset_short_name" = query[[1]]$details$suffix[which(query[[1]]$details$prefix == "dataset_short_name")],
+    "product_type" = query[[1]]$details$suffix[which(query[[1]]$details$prefix == "product_type")],
+    "variable" = query[[1]]$details$suffix[which(query[[1]]$details$prefix == "variable")],
+    "pressure_level" = query[[1]]$details$suffix[which(query[[1]]$details$prefix == "pressure_level")],
+    "year" = query[[1]]$details$suffix[which(query[[1]]$details$prefix == "year")],
+    "month" = query[[1]]$details$suffix[which(query[[1]]$details$prefix == "month")],
+    "day" = query[[1]]$details$suffix[which(query[[1]]$details$prefix == "day")],
+    "time" = query[[1]]$details$suffix[which(query[[1]]$details$prefix == "time")],
+    "area" = query[[1]]$details$suffix[which(query[[1]]$details$prefix == "area")],
+    "format" = query[[1]]$details$suffix[which(query[[1]]$details$prefix == "format")],
+    "target" = query[[1]]$details$suffix[which(query[[1]]$details$prefix == "target")]
+  )
+  return(query_request)
+}
+
+#' Check a folder for met queries
+#'
+#' @description Check a folder for query files.
+#'
+#' @param query_folder full file path to the folder containing queries.
+#' @param target_string character vector of a string used to grep the file names. Use to specify or isolate from other files.
+#'
+#' @noRd
+#'
+check_for_queries <- function(query_folder = paste0(getwd(),"/incoming-queries/"),
+                              target_string = 'query'){
+  # query_folder = paste0(proj_test_dir,"incoming-queries/")
+  # target_string = 'query'
+  # Are there files in the folder?
+  query_files <- list.files(query_folder, full.names = T)
+  if(is_empty(query_files)){
+    return(FALSE)
+  } else {
+    # Check for string query
+    if(!is.null(target_string)){
+      queries_short <- list.files(query_folder, full.names = F)
+      matches <- grep(target_string,queries_short)
+      queries <- query_files[c(matches)]
+      if(is_empty(queries)){
+        return(FALSE) # There are no entries that match the string query.
+      } else {
+        return(TRUE) # There are entries that match the string query.
+      }
+    } else {
+      # There is no match string, and there are files.
+      return(TRUE)
+    }
+  }
+}
 
 #' Create a query table
 #'
@@ -16,9 +105,8 @@
 #' @param time A vector containing one or more times in the format "HH:MM" e.g. "03:00"; request var for ecmwfr
 #' @param area An area definition for the met. Format "NORTH/WEST/SOUTH/EAST" e.g. "0/-180/-90/180" for the entire SH.
 #' @param format What format? One of 'grib' or 'netcdf'
-
-create_met_query <- function(output_dir = outgoing_met_queries_dir,
-                             pc_name,
+#'
+create_met_query <- function(pc_name,
                              dataset_short_name = "reanalysis-era5-pressure-levels",
                              product_type = "reanalysis",
                              variable = all_vars,
@@ -28,12 +116,21 @@ create_met_query <- function(output_dir = outgoing_met_queries_dir,
                              day = "04",
                              time = times_3_hours,
                              area = "0/-180/-90/180",
-                             format = "grib"){
-
+                             format = "grib",
+                             target = "target"){
+  if(format == "grib"){
+    if(isFALSE(grepl(paste0(".",format),target))){
+      target = paste0(target,".",format)
+    }
+  } else if(format == "netcdf"){
+    if(isFALSE(grepl(".nc",target))){
+      target = paste0(target,".nc")
+    }
+  }
   # create table with relevant info.
   # adopting the approach of the SampleQueue log file creation method.
   ## Create blank query
-  blank_query <- data.frame(matrix(NA,nrow = 18, ncol = 2)) %>% 'colnames<-'(c('prefix','suffix'))
+  blank_query <- data.frame(matrix(NA,nrow = 19, ncol = 2)) %>% 'colnames<-'(c('prefix','suffix'))
   blank_query[1,1] <- c("MET_QUERY")
   blank_query[3,1] <- c("metfetchR_package")
   # BREAKS
@@ -48,8 +145,8 @@ create_met_query <- function(output_dir = outgoing_met_queries_dir,
   # DETAILS
   blank_query[8,1] <- c("DETAILS")
   # QUERY INFO
-  vars <- c("dataset_short_name","product_type","variable","pressure_level","year","month","day","time", "area",'format')
-  blank_query[seq(9,18,1),1] <- vars
+  vars <- c("dataset_short_name","product_type","variable","pressure_level","year","month","day","time", "area",'format','target')
+  blank_query[seq(9,19,1),1] <- vars
   # Assign inputs using explicit name indexing
   blank_query[which(blank_query[,1] == "dataset_short_name"),2] <- dataset_short_name
   blank_query[which(blank_query[,1] == "product_type"),2] <- product_type
@@ -61,6 +158,7 @@ create_met_query <- function(output_dir = outgoing_met_queries_dir,
   blank_query[which(blank_query[,1] == "time"),2] <- paste0(time, collapse = ",")
   blank_query[which(blank_query[,1] == "area"),2] <- area
   blank_query[which(blank_query[,1] == "format"),2] <- format
+  blank_query[which(blank_query[,1] == "target"),2] <- target
   return(blank_query)
 }
 
@@ -107,9 +205,12 @@ save_query <- function(query_table,
 #' @param which_element numeric vector of length >=1 or 'all', used to index the query files. By default only the first is selected.
 #' @param target_string character vector of a string used to grep the file names. Use to specify or isolate from other files.
 #'
+#' @importFrom rlang is_empty
+#' @importFrom magrittr %>%
+#'
 #' @noRd
 #'
-read_met_query <- function(query_dir = paste0(here(),"/incoming-queries/"),
+read_met_query <- function(query_dir = paste0(getwd(),"/incoming-queries/"),
                            which_element = 1,
                            target_string = 'query'){
   # test vars
@@ -164,7 +265,7 @@ read_met_query <- function(query_dir = paste0(here(),"/incoming-queries/"),
 parse_met_query <- function(query_file,
                             warn = FALSE){
   # Create results list
-  res_list <- vector('list', length = 3) %>%
+  res_list <- vector('list', length = 4) %>%
     'names<-'(c('file','from','created','details'))
   res_list$file <- query_file
   # fl <- read.table(file = query_file, sep = '\t')
--- R/utils.R
+++ R/utils.R
@@ -64,3 +64,29 @@ is_date <- function(mydate, date.format = "%d/%m/%y"){
   tryCatch(!is.na(as.Date(mydate, date.format)),
            error = function(err) {FALSE})
 }
+
+#' Remove the file path from a given file or folder name.
+#'
+#' @description Removes the file or folder path from a given file/folder name by splitting the path at every "/", and getting rid of all but the last set of characters. If given a short filename (no path), the same string will just be spat out the other end unchanged.
+#'
+#' @param filnames a string containing the full file path.
+#'
+#' @noRd
+#'
+trim_path <- function(filenames){
+  if(length(filenames) > 1){
+    it_list <- vector(mode = "list", length = length(filenames))
+    trimmed_filenames <- vector(mode = "character", length = length(filenames))
+    for(f in seq_along(filenames)){
+      filename_trimmed <- unlist(strsplit(filenames[f],"/"))[length(unlist(strsplit(filenames[f],"/")))]
+      trimmed_filenames[f] <- filename_trimmed
+    }
+    trimmed_filenames
+  } else if(length(filenames) == 1){
+    filename <- filenames
+    filename_trimmed <- unlist(strsplit(filename,"/"))[length(unlist(strsplit(filename,"/")))]
+    filename_trimmed
+  } else{
+    message("Empty object; no path to trim.")
+  }
+}
